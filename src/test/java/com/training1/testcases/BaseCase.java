1405c2118273631b3f05074afac84b29b9cdf506
denisha-addweb <denisha.addweb@gmail.com>
2019-11-03 11:11:14 +0530

    Base case changes

--- src/test/java/com/training1/testcases/BaseCase.java
+++ src/test/java/com/training1/testcases/BaseCase.java
@@ -5,6 +5,7 @@ import com.training1.constants.CommonVar;
 import com.training1.reports.ReportClass;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.*;
 public class BaseCase extends ReportClass {
 
@@ -17,8 +18,11 @@ public class BaseCase extends ReportClass {
     public void initialize() throws SQLException
     {
         System.setProperty("Webdriver.chrome.driver",System.getProperty("user.dir")+"chromedriver");
+        ChromeOptions options=new ChromeOptions();
+        options.addArguments("--remote-debugging-port=9222");
+
         //System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"\\chromedriver.exe" );
-        driver = new ChromeDriver();
+        driver = new ChromeDriver(options);
         //driver.get("http://toolsqa.com/automation-practice-form/");
         //driver.get("http://toolsqa.com/automation-practice-table/");
         //driver.get("http://toolsqa.com/handling-alerts-using-selenium-webdriver/");
