e58e166cf9d00c4d0153affb1e09d57513d3bdba
denisha-addweb <denisha.addweb@gmail.com>
2020-02-22 23:28:26 +0530

    test case changes

--- src/test/java/com/training1/testcases/BaseCase.java
+++ src/test/java/com/training1/testcases/BaseCase.java
@@ -17,7 +17,7 @@ public class BaseCase extends ReportClass {
     @BeforeMethod
     public void initialize() throws SQLException
     {
-        //System.setProperty("Webdriver.chrome.driver",System.getProperty("user.dir")+"chromedriver");
+        System.setProperty("Webdriver.chrome.driver",System.getProperty("user.dir")+"chromedriver");
         ChromeOptions options=new ChromeOptions();
         options.addArguments("--remote-debugging-port=9222");
         options.addArguments("headless");
