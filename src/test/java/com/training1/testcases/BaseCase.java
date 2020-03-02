bac8847105aee8706bfaea3c65b895cf890cc1fc
krina-soni <krinaadeshra@gmail.com>
2020-03-03 00:00:07 +0530

    test case changes

--- src/test/java/com/training1/testcases/BaseCase.java
+++ src/test/java/com/training1/testcases/BaseCase.java
@@ -19,6 +19,7 @@ public class BaseCase extends ReportClass {
             {
                 System.setProperty("Webdriver.chrome.driver",System.getProperty("user.dir")+"chromedriver");
                 ChromeOptions options=new ChromeOptions();
+                options.addArguments("--no-sandbox","--disable-dev-shm-usage"); // overcome limited resource problems
                 options.addArguments("--remote-debugging-port=9222");
                 options.addArguments("headless");
                 options.setExperimentalOption("useAutomationExtension", false);
@@ -26,7 +27,6 @@ public class BaseCase extends ReportClass {
                 options.addArguments("disable-infobars"); // disabling infobars
                 options.addArguments("--disable-extensions"); // disabling extensions
                 options.addArguments("--disable-gpu"); // applicable to windows os only
-                options.addArguments("--no-sandbox","--disable-dev-shm-usage"); // overcome limited resource problems
                 // Bypass OS security model
                // WebDriver driver = new ChromeDriver(options);
 
