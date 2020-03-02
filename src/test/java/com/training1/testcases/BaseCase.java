3c2774de7ccb035fbd115697388a4e09322555f6
krina-soni <krinaadeshra@gmail.com>
2020-03-02 23:29:46 +0530

    test case changes

--- src/test/java/com/training1/testcases/BaseCase.java
+++ src/test/java/com/training1/testcases/BaseCase.java
@@ -21,6 +21,13 @@ public class BaseCase extends ReportClass {
                 ChromeOptions options=new ChromeOptions();
                 options.addArguments("--remote-debugging-port=9222");
                 options.addArguments("headless");
+                options.addArguments("start-maximized"); // open Browser in maximized mode
+                options.addArguments("disable-infobars"); // disabling infobars
+                options.addArguments("--disable-extensions"); // disabling extensions
+                options.addArguments("--disable-gpu"); // applicable to windows os only
+                options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+                options.addArguments("--no-sandbox"); // Bypass OS security model
+                WebDriver driver = new ChromeDriver(options);
 
                 //System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"\\chromedriver.exe" );
                 //driver = new ChromeDriver(options);
