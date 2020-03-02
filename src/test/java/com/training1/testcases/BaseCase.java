f82396e58381019e8c35232d1dc236f34f3b23ef
krina-soni <krinaadeshra@gmail.com>
2020-03-02 23:53:48 +0530

    test case changes

--- src/test/java/com/training1/testcases/BaseCase.java
+++ src/test/java/com/training1/testcases/BaseCase.java
@@ -25,9 +25,9 @@ public class BaseCase extends ReportClass {
                 options.addArguments("disable-infobars"); // disabling infobars
                 options.addArguments("--disable-extensions"); // disabling extensions
                 options.addArguments("--disable-gpu"); // applicable to windows os only
-                options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-                options.addArguments("--no-sandbox"); // Bypass OS security model
-                WebDriver driver = new ChromeDriver(options);
+                options.addArguments("--no-sandbox","--disable-dev-shm-usage"); // overcome limited resource problems
+                // Bypass OS security model
+               // WebDriver driver = new ChromeDriver(options);
 
                 //System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"\\chromedriver.exe" );
                 //driver = new ChromeDriver(options);
